9e9e5fff59c88d6712a3c735b512d611387c2d42
Mansi Gupta <mansigupta.codes@gmail.com>
2026-03-16 22:53:07 +0530

    Initial commit

---
+++ Day-1/app.js
@@ -0,0 +1,8 @@
+const express = require("express")
+
+const app = express() // server create ho gya h yaha
+
+app.listen(3000, () => {
+    console.log('server started on port 3000')
+}) // server ko start krta h
+
