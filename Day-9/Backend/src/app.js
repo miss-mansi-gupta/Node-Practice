67700d59acb58e3fef8233d368a74846556d4287
Mansi Gupta <mansigupta.codes@gmail.com>
2026-03-27 00:41:42 +0530

    FullStack mini task

---
+++ Day-9/Backend/src/app.js
@@ -0,0 +1,64 @@
+// server ko create krna
+// server ko config krna
+
+const express = require("express")
+const noteModel = require("./models/notes.model")
+const cors = require("cors")
+
+const app = express()
+
+
+app.use(cors()) // accepts cross origin requests
+app.use(express.json()) // using middleware
+
+// POST /api/notes
+// create new note and save data in mongob
+// req.body -> title , description
+
+app.post('/api/notes', async(req, res) => {
+    const { title, description } = req.body
+    const note = await noteModel.create({
+        title, description
+    })
+    res.status(201).json({
+        message: "Note created successfully",
+        note
+    })
+})
+
+// GET /api/notes
+// fetch all notes data from mongodb and send them in response
+
+app.get("/api/notes", async (req, res) => {
+    const notes = await noteModel.find() // find method fetches array of objects
+    res.status(200).json({
+        message: "Notes fetched successfully",
+        notes
+    })
+})
+
+// DELETE /api/notes/:id
+// Delete note with the id from req.params
+
+app.delete("/api/notes/:id", async (req, res) => {
+    const id = req.params.id
+    await noteModel.findByIdAndDelete(id)
+    res.status(200).json({
+        message: "Note deleted successfully",
+    }) 
+})
+
+// PATCH /api/notes/:id
+// Update the description of the note by id
+// req.body -> description
+
+app.patch("/api/notes/:id", async (req, res) => {
+    const id = req.params.id
+    const { description } = req.body
+    await noteModel.findByIdAndUpdate(id, { description })
+    res.status(200).json({
+        message: "Note updtaed successfully"
+    })
+})
+
+module.exports = app;
---
+++ Day-9/Backend/src/models/notes.model.js
@@ -0,0 +1,12 @@
+// creating schema and model
+
+const mongoose = require("mongoose")
+
+const noteSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+})
+
+const noteModel = mongoose.model("notes", noteSchema)
+
+module.exports = noteModel
