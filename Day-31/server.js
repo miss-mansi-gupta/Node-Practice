c279058540b44e6ecc281c32fe3d1b6a52c1d9b2
Mansi Gupta <mansigupta.codes@gmail.com>
2026-08-09 13:16:22 +0530

    Day-31 Socket.io

---
+++ Day-31/server.js
@@ -0,0 +1,36 @@
+import app from "./src/app.js";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, { /* options */ });
+
+
+// io.on => server ko listen krna
+// jb server pe naya connection bnega to ye callback chlega
+
+io.on("connection", (socket) => {
+    console.log("new connection created")
+    // socket.on => single user msg event ko fire krega tb event listen kr lege
+    socket.on("message", (msg) => {
+        console.log("user fired message event")
+        console.log(msg)
+
+        // io.emit => server ko fire krna
+        io.emit("abc", msg)
+    })
+})
+
+//task study
+// socket.emit()
+// socket.broadcast().emit()
+// io.emit() 
+// socket.io documentation => events, adapters, client, server
+
+httpServer.listen(3000, () => {
+    console.log("Server is running on port 3000")
+});
+
+// app.listen(3000, () => {
+//     console.log("Server is running on port 3000")
+// })
